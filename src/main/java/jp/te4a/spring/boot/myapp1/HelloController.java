77fe99c9462fe19decb9c268fb9ed518fa997ba9
Your Name <you@example.com>
2024-09-06 16:46:33 +0900

    課題1初回登録

--- src/main/java/jp/te4a/spring/boot/myapp1/HelloController.java
+++ src/main/java/jp/te4a/spring/boot/myapp1/HelloController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
  
 /**
- * Hello world!
+ * Hello world2!
  *
  */
 @RestController
